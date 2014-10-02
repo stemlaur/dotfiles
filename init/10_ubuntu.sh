3d45eeeba127372640de4112830a9eee9b3e2e22
Laurent Stemmer <laurent.stemmer@gmail.com>
2014-10-02 13:52:12 +0200

    vim config

--- init/10_ubuntu.sh
+++ init/10_ubuntu.sh
@@ -2,33 +2,7 @@
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
 
 # If the old files isn't removed, the duplicate APT alias will break sudo!
-sudoers_old="/etc/sudoers.d/sudoers-cowboy"; [[ -e "$sudoers_old" ]] && sudo rm "$sudoers_old"
-
-# Installing this sudoers file makes life easier.
-sudoers_file="sudoers-dotfiles"
-sudoers_src=~/.dotfiles/conf/ubuntu/$sudoers_file
-sudoers_dest="/etc/sudoers.d/$sudoers_file"
-if [[ ! -e "$sudoers_dest" || "$sudoers_dest" -ot "$sudoers_src" ]]; then
-  cat <<EOF
-The sudoers file can be updated to allow certain commands to be executed
-without needing to use sudo. This is potentially dangerous and should only
-be attempted if you are logged in as root in another shell.
-
-This will be skipped if "Y" isn't pressed within the next 15 seconds.
-EOF
-  read -N 1 -t 15 -p "Update sudoers file? [y/N] " update_sudoers; echo
-  if [[ "$update_sudoers" =~ [Yy] ]]; then
-    e_header "Updating sudoers"
-    visudo -cf "$sudoers_src" >/dev/null && {
-      sudo cp "$sudoers_src" "$sudoers_dest" &&
-      sudo chmod 0440 "$sudoers_dest"
-    } >/dev/null 2>&1 &&
-    echo "File $sudoers_dest updated." ||
-    echo "Error updating $sudoers_dest file."
-  else
-    echo "Skipping."
-  fi
-fi
+sudo -v
 
 # Update APT.
 e_header "Updating APT"
@@ -42,6 +16,7 @@ packages=(
   tree sl id3tool cowsay
   nmap telnet
   htop
+  ruby
 )
 
 list=()
--- init/50_devel.sh
+++ init/50_devel.sh
@@ -31,19 +31,6 @@ if [[ "$(type -P npm)" ]]; then
   fi
 fi
 
-# Install Ruby.
-if [[ "$(type -P rbenv)" ]]; then
-  versions=(2.0.0-p247)
-
-  list="$(to_install "${versions[*]}" "$(rbenv whence ruby)")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby versions: $list"
-    for version in $list; do rbenv install "$version"; done
-    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
-    rbenv rehash
-  fi
-fi
-
 # Install Gems.
 if [[ "$(type -P gem)" ]]; then
   gems=(bundler awesome_print pry lolcat)
